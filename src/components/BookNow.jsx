fdaa3a381847e12dbb1430b57da6f0385e3a9c73
omkarr10 <pvtomkar72@gmail.com>
2025-06-20 01:19:44 +0530

    Update BookNow.jsx

--- src/components/BookNow.jsx
+++ src/components/BookNow.jsx
@@ -69,7 +69,7 @@ const BookNow = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/bookings", {
+      const response = await fetch("https://vgbnd.onrender.com/api/bookings", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
